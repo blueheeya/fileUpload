74e1721c4c51b0c3a446d174594c7e067c2d55f1
blueheeya83 <blueheeya12@gmail.com>
2024-03-25 11:45:21 +0900

    first commit

---
+++ src/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express;
+const dotenv = require("dotenv");
+const { default: mongoose } = require("mongoose");
+const multer = require("multer");
+const upload = multer({dest:"uploads"})
+dotenv.config();
+
+const server = async function(){
+
+}
+
+server();
