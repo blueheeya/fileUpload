f42163486dd1d8cf60842d97bd792fb271386cdd
blueheeya83 <blueheeya12@gmail.com>
2024-03-25 18:08:54 +0900

    20240325

--- src/server.js
+++ src/server.js
@@ -1,13 +1,39 @@
 const express = require("express");
-const app = express;
+const {default: mongoose} = require("mongoose");
+const app = express();
 const dotenv = require("dotenv");
-const { default: mongoose } = require("mongoose");
-const multer = require("multer");
-const upload = multer({dest:"uploads"})
+//imgeUpload로 빼냄 
+// const multer = require("multer");
+// const {v4: uuid} = require("uuid");
+// const mime = require("mime-types");
+const {upload} = require("./middlewares/imageUpload")
+
+
 dotenv.config();
 
-const server = async function(){
+//app.use( express.static("uploads"));
+//http://localhost:3000/abc.jpeg
+app.use("/uploads", express.static("uploads"));
+//http://localhost:3000/uploads/abc.jpeg
 
-}
-
-server();
+const server = async function () {
+    try {
+      await mongoose.connect(process.env.MONGO_URL);
+      console.log("db연결됨");
+      app.use(express.json());
+  
+      app.post("/upload", upload.single("image"), async function (req, res) {
+        try {
+          console.log(req.file);
+          return res.send(req.file);
+        } catch (error) {
+          return res.status(500).send({error: error.message});
+        }
+      });
+  
+      app.listen(3000);
+    } catch (error) {
+      console.log("연결안됨");
+    }
+  };
+  server();
